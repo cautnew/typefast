74076a3daf89a89ead4a4cb88f2e558c6207b04b
Felipe Martins <felipedesmartins@gmail.com>
2023-10-26 08:27:32 -0300

    Timer set in miliseconds

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const txtUser = document.querySelector('#text-user');
 const txtExample = document.querySelector("#text-example");
 const btnRestart = document.querySelector("#btn-restart");
+const spanTimer = document.querySelector("#span-timer");
 
 const gabaritos = [
   "A chuva cai suavemente, tocando a terra com carinho, trazendo vida e renovação a cada gota que desce do céu.",
@@ -17,7 +18,27 @@ const totalWordsCurrentGabarito = currentGabarito.split(' ').length;
 
 var interval;
 var timerRunning = false;
+var hundredths;
+
+const startTimer = () => {
+  timerRunning = true;
+  hundredths = 0;
+  interval = setInterval(timerClock, 10);
+};
+
+const timerClock = () => {
+  const cents = hundredths/100;
+  const min = Math.floor(cents/60);
+  const sec = Math.floor(cents - min * 60);
+  const msc = Math.floor(hundredths - sec * 100 - min * 6000);
+  const currentTime = min + ":" + sec + "." + msc;
+
+  spanTimer.innerText = currentTime;
+
+  hundredths ++;
+};
 
 window.addEventListener('load', function () {
   txtExample.innerText = currentGabarito;
+  startTimer();
 });
