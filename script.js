9cf1634a24d3a86f1e73af27dfabaa946ab01058
Felipe Martins <felipedesmartins@gmail.com>
2023-10-26 23:16:46 -0300

    Added progress bar

--- script.js
+++ script.js
@@ -2,15 +2,8 @@ const txtUser = document.querySelector('#text-user');
 const txtExample = document.querySelector("#text-example");
 const spanTimer = document.querySelector("#span-timer");
 const btnRestart = document.querySelector("#btn-restart");
-
-const gabaritos = [
-  "A chuva cai suavemente, tocando a terra com carinho, trazendo vida e renovação a cada gota que desce do céu.",
-  "Em dias chuvosos, a natureza canta uma melodia serena, convidando-nos a refletir e apreciar sua beleza.",
-  "Cada pingo de chuva conta uma história, unindo-se para criar rios, alimentar florestas e abraçar a terra."
-];
-
-const getRandomInt = (valMax) => Math.floor(Math.random() * valMax);
-const maxGabaritos = gabaritos.length;
+const spcProgress = document.querySelector(".progress");
+const progressBar = spcProgress.querySelector(".progress-bar");
 
 var interval;
 var timerRunning = false;
@@ -21,16 +14,15 @@ var totalCharsCurrentGabarito = 0;
 var currentGabarito = "";
 var txtTyped = "";
 
-const startTimer = () => {
-  timerRunning = true;
-  hundredths = 0;
-  interval = setInterval(timerClock, 10);
-};
+const gabaritos = [
+  "A chuva cai suavemente, tocando a terra com carinho, trazendo vida e renovação a cada gota que desce do céu.",
+  "Em dias chuvosos, a natureza canta uma melodia serena, convidando-nos a refletir e apreciar sua beleza.",
+  "Cada pingo de chuva conta uma história, unindo-se para criar rios, alimentar florestas e abraçar a terra."
+];
 
-const stopTimer = () => {
-  clearInterval(interval);
-  timerRunning = false;
-};
+const maxGabaritos = gabaritos.length;
+
+const getRandomInt = (valMax) => Math.floor(Math.random() * valMax);
 
 const leadingZero = (num = 0) => {
   if (num <= 9) {
@@ -43,6 +35,32 @@ const leadingZero = (num = 0) => {
 const timerClock = () => {
   updateTimer();
   hundredths ++;
+};
+
+const startTimer = () => {
+  timerRunning = true;
+  hundredths = 0;
+  interval = setInterval(timerClock, 10);
+};
+
+const stopTimer = () => {
+  clearInterval(interval);
+  timerRunning = false;
+};
+
+const updateProgressBar = (percent) => {
+  spcProgress.setAttribute('aria-valuenow', percent);
+  progressBar.style.width = percent + "%";
+};
+
+const getGabarito = () => {
+  indexCurrentGabarito = getRandomInt(maxGabaritos);
+  return gabaritos[indexCurrentGabarito];
+};
+
+const loadStatistics = () => {
+  totalCharsCurrentGabarito = currentGabarito.length;
+  totalWordsCurrentGabarito = currentGabarito.split(' ').length;
 };
 
 const updateTimer = () => {
@@ -59,25 +77,21 @@ const updateTimer = () => {
   spanTimer.innerText = leadingZero(min) + ":" + leadingZero(sec) + "." + leadingZero(msc);
 };
 
-const getGabarito = () => {
-  indexCurrentGabarito = getRandomInt(maxGabaritos);
-  return gabaritos[indexCurrentGabarito];
-};
-
-const loadStatistics = () => {
-  totalCharsCurrentGabarito = currentGabarito.length;
-  totalWordsCurrentGabarito = currentGabarito.split(' ').length;
-};
-
 const startGame = () => {
   const txtTyped = txtUser.value;
   const charsTyped = txtTyped.length;
 
+  btnRestart.innerText = "Reiniciar jogo";
   txtUser.setAttribute('maxlength', totalCharsCurrentGabarito);
 
   if (charsTyped === 0 && !timerRunning) {
     startTimer();
   }
+};
+
+const endGame = () => {
+  stopTimer();
+  txtUser.setAttribute('disabled', 'disabled');
 };
 
 const resetGame = () => {
@@ -89,15 +103,23 @@ const resetGame = () => {
   txtExample.innerText = currentGabarito;
   txtUser.value = '';
   txtUser.removeAttribute('disabled');
+  updateProgressBar(0);
 };
 
 const resultCheck = () => {
   txtTyped = txtUser.value;
   const qtdCharsTyped = txtTyped.length;
+  const percentTyped = Math.round(qtdCharsTyped / totalCharsCurrentGabarito * 100);
+
+  updateProgressBar(percentTyped);
 
   if (txtTyped == currentGabarito) {
-    stopTimer();
-    txtUser.setAttribute('disabled', 'disabled');
+    progressBar.classList.add('bg-primary');
+    progressBar.classList.remove('bg-danger');
+    endGame();
+  } else {
+    progressBar.classList.add('bg-danger');
+    progressBar.classList.remove('bg-primary');
   }
 };
 
